225ae579e78ab2e33fc5bd069e2d7a4f0d6d5cb6
Nikhil-Arjun <nikhilarjun97@gmail.com>
2024-03-28 12:08:12 +0530

    project update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./layout.scss";
+import HomePage from "./routes/Home-Section/HomePage";
 
 const App = () => {
   return (
     <div className="layout">
-      <Navbar />
+      <div className="navbar">
+        <Navbar />
+      </div>
+
+      <div className="content">
+        <HomePage />
+      </div>
     </div>
   );
 };
